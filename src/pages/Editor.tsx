61fbfe93a84706170f6b08061bce3ec548ae9c52
Oskar <oskar.seierl@aon.at>
2026-05-31 19:36:43 +0200

    feat: resizeable container for editor

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,14 +1,26 @@
 import {Image} from "../../shared/types/image.ts";
 import {ImageLibrary} from "@/components/ImageLibrary.tsx";
+import {useState} from "react";
+import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/components/ui/resizable.tsx";
 
 interface EditorProps {
   images: Image[];
 }
 
 export function Editor({images}: EditorProps) {
+  const [selectedImages, setSelectedImages] = useState<Image[]>([]);
+
   return (
-    <div>
-      <ImageLibrary images={images} onSelectedChange={(ids) => console.log(ids)} />
-    </div>
+    <ResizablePanelGroup>
+      <ResizablePanel defaultSize="75%" minSize="600px">
+        <ImageLibrary images={images} onSelectedChange={setSelectedImages}/>
+      </ResizablePanel>
+      <ResizableHandle withHandle/>
+      <ResizablePanel defaultSize="25%">
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">Content</span>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
   );
 }
